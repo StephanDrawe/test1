9d8327942a9c3ed29f9ce36087a142eaab714e60
Stephan Drawe <stephan.drawe@hotmail.com>
2022-12-12 14:42:24 -0600

    added mapanimation

---
+++ week9/mapanimation.js
@@ -0,0 +1,38 @@
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2RyYXdlIiwiYSI6ImNsYmZsbXB0YTA3ZHgzdmxhYXl4Z2c4N3QifQ.rw99q1mMjaB2nLBBOY1Fmw';
+    
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-73.984016, 40.754932],
+    zoom: 11
+});
+
+var marker = new mapboxgl.Marker()
+.setLngLat([, ])
+.addTo(map);
+
+const busStops = [
+    [-71.093729, 42.359244],
+    [-71.094915, 42.360175],
+    [-71.095800, 42.360698],
+    [-71.099558, 42.362953],
+    [-71.103476, 42.365248],
+    [-71.106067, 42.366806],
+    [-71.108717, 42.368355],
+    [-71.110799, 42.369192],
+    [-71.113095, 42.370218],
+    [-71.115476, 42.372085],
+    [-71.117585, 42.373016],
+    [-71.118625, 42.374863]
+];
+
+var counter = 0;
+function move(){
+    setTimeout(()=>{
+    if (counter >= busStops.length) return;
+    marker.setLngLat(busStops[counter]);
+    counter++;
+    move();
+    }, 1000);
+}
